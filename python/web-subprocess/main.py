099434ef5c9b2dfd269384deb8d1b9cb1064a6aa
iganari <cigaguri@gmail.com>
2022-02-13 14:17:49 +0900

    port の設定を環境変数に入れる

--- python/web-subprocess/main.py
+++ python/web-subprocess/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 import subprocess
 import datetime
+import os
+
+container_port = int(os.environ['PORT'])
 
 app = Flask(__name__)
 
@@ -18,5 +21,5 @@ def hello():
     return msg
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=container_port)
     
