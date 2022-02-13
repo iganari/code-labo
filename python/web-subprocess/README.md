92d95558d6ab3698a4348cfe3c5e0ea5c3e69f29
iganari <cigaguri@gmail.com>
2022-02-13 14:12:54 +0900

    rename

---
+++ python/web-subprocess/README.md
@@ -0,0 +1,44 @@
+# Python: Web SubProcess
+
+## Docker コンテナを用いて開発する時
+
+```
+cd code-labo/python/web-subprocess
+```
+
++ コンテナの作成
+
+```
+docker build . --tag codelabo-py-websubprocess
+```
+
++ コンテナをデーモン起動
+
+```
+docker run \
+  -d \
+  -p 8080:5000 \
+  -v $(pwd):/app \
+  --name codelabo-py-websubprocess \
+  codelabo-py-websubprocess
+```
+
++ Web ブラウザから確認
+
+http://0.0.0.0:8080
+
+WIP
+
++ [debug] コンテナにログイン ---> :whale:
+
+```
+docker exec -it codelabo-py-websubprocess /bin/bash
+```
+
++ コンテナの作り直し OR 削除
+
+```
+docker stop codelabo-py-websubprocess && \
+docker rm -f codelabo-py-websubprocess && \
+docker rmi codelabo-py-websubprocess
+```
--- python/web-subprocess/main.py
+++ python/web-subprocess/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import subprocess
+import datetime
 
 app = Flask(__name__)
 
@@ -7,10 +8,14 @@ app = Flask(__name__)
 def hello():
 
     _ex_remote_addr = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    # Comment on standard output in Container.
     subprocess.call(["echo", "Start 10s"])
+    # Execute the Sleep command.
     subprocess.call(["sleep", "10"])
-    msg ='Your Remote IP Address is "{}" .\nHello World!! :)'.format(_ex_remote_addr)
-    return 'test done'
+
+    msg ='test is done.\n It is "{}" now.'.format(datetime.datetime.now())
+
+    return msg
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
